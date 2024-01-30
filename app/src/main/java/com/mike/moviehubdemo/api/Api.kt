ecf6e3e87c5c4a4cfc4395e3e66e1e580f2af09f
mbialowas10 <mbialowas@rrc.ca>
2024-01-30 10:41:02 -0600

    finalized api files for api communication

---
+++ app/src/main/java/com/mike/moviehubdemo/api/Api.kt
@@ -0,0 +1,27 @@
+package com.mike.moviehubdemo.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object Api {
+    private val BASE_URL = "https://api.themoviedb.org/3/"
+
+    // convert json response into objects that the project models can understand
+    private val moshi  = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    // initialization of retrofit instance
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val retrofitService: MovieService by lazy{
+        // instantiate our retrofit service ie. the movieService
+        retrofit.create(MovieService::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/mike/moviehubdemo/api/MoviesManager.kt
@@ -0,0 +1,50 @@
+package com.mike.moviehubdemo.api
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.mike.moviehubdemo.model.Movie
+import com.mike.moviehubdemo.model.MovieData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MoviesManager {
+    private var _movieResponse = mutableStateOf<List<Movie>>(emptyList())
+    val api_key:String = "105eb33fc0b1d90e4346524a7e7a778e"
+
+    val movieResponse: MutableState<List<Movie>>
+        @Composable get() = remember{
+            _movieResponse
+        }
+
+    init{
+        getMovies()
+    }
+
+    private fun getMovies(){
+        val service = Api.retrofitService.getTrendingMovies(api_key)
+
+        service.enqueue(object : Callback<MovieData>{
+            override fun onResponse(
+                call: Call<MovieData>,
+                response: Response<MovieData>
+            ) {
+                if (response.isSuccessful){
+                    Log.i("Data", "Data Loaded")
+                    _movieResponse.value = response.body()?.results?: emptyList()
+                    Log.i("DataStream", _movieResponse.toString())
+
+                }
+            }
+
+            override fun onFailure(call: Call<MovieData>, t: Throwable) {
+                Log.d("error", "${t.message}")
+            }
+
+        })
+    }
+}
+
