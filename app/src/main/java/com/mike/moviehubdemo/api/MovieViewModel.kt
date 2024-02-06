2e5998e9ba43e9b4415f80aa8553015515861f02
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-02-06 11:04:30 -0600

    Added MovieViewModel, updated service and search form

---
+++ app/src/main/java/com/mike/moviehubdemo/api/MovieViewModel.kt
@@ -0,0 +1,52 @@
+package com.mike.moviehubdemo.api
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import com.mike.moviehubdemo.model.Movie
+import com.mike.moviehubdemo.model.MovieData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MovieViewModel : ViewModel() {
+
+    val movies = mutableStateOf<List<Movie>>(emptyList())
+
+    val moviesResponse: MutableState<List<Movie>> // state allows us to make data available within app
+        @Composable get() = remember{
+            movies
+        }
+
+        val api_key:String = "df2c0933d248a9c91c0ed01e25054bd1"
+
+        fun searchMovies(movieName: String){
+            if (movieName.isNotBlank()){
+                // this is how we call the search Movie endpoint specified in MoviesService interface (api call)
+                val service = Api.retrofitService.searchMovieByName(api_key,movieName)
+                service.enqueue(object : Callback<MovieData>{
+                    override fun onResponse(call: Call<MovieData>, response: Response<MovieData>) {
+                        if (response.isSuccessful){
+                            Log.i("MJB", "testing testing")
+                            movies.value = response.body()?.results?:emptyList()
+                            Log.i("MovieFound", movies.toString())
+                        }
+                    }
+
+                    override fun onFailure(call: Call<MovieData>, t: Throwable) {
+                        Log.d("error", "${t.message}")
+                    }
+                })
+
+
+            }else{
+                movies.value = emptyList()
+            }
+        }
+
+
+
+}
--- app/src/main/java/com/mike/moviehubdemo/view/MovieSearchForm.kt
+++ app/src/main/java/com/mike/moviehubdemo/view/MovieSearchForm.kt
@@ -3,29 +3,44 @@ package com.mike.moviehubdemo.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
+
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.mike.moviehubdemo.api.MovieViewModel
+import com.mike.moviehubdemo.components.MovieCard
 import com.mike.moviehubdemo.model.Movie
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MovieSearchForm(navController: NavController){
     val context = LocalContext.current
     val keyboardController = LocalSoftwareKeyboardController.current
-    // todo add in val viewModel:MovieViewModel = viewModel()
-    //val movies: List<Movie> = movies.value
+    val viewModel: MovieViewModel = viewModel()
+    val movies: List<Movie> = viewModel.movies.value // get searched movies
 
-    var query by remember { mutableState("")}
+
+    var query by remember { mutableStateOf("")}
 
     Column(
         modifier = Modifier
@@ -37,13 +52,35 @@ fun MovieSearchForm(navController: NavController){
             value =  query,
             onValueChange = { query = it },
             label = {Text("Search for a movie")},
-            keyboardOptions = KeyboardOptions(imeAction= ImeAction.Search)
-//            keyboardActions  KeyboardActions(onSearch = {
-//                // add view model in a bit
-//            }),
+            keyboardOptions = KeyboardOptions(imeAction= ImeAction.Search),
+            keyboardActions = KeyboardActions(onSearch = {
+                // add view model in a bit
+              viewModel.searchMovies(query)
+              keyboardController?.hide()
+            }),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom=16.dp)
+                .padding(bottom = 16.dp)
         )
+        Button(
+            onClick ={
+                // view model to go next
+                viewModel.searchMovies(query)
+            }
+        ){
+            Text("Search")
+        }
+        // thread aka corountine in android
+        LaunchedEffect(viewModel){
+            viewModel.searchMovies(query)
+        }
+
+        LazyColumn{
+            items(movies){ movie ->
+                MovieCard(movieItem = movie, navController = navController)
+
+            }
+        }
+
     }
 }
