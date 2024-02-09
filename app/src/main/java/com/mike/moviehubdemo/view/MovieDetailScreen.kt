6c9cfe9cb294f31e1d25e15b81c61e09afd1fb84
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-02-09 14:14:55 -0600

    Fake Movie data being passed to Movie Detail screen

--- app/src/main/java/com/mike/moviehubdemo/view/MovieDetailScreen.kt
+++ app/src/main/java/com/mike/moviehubdemo/view/MovieDetailScreen.kt
@@ -1,12 +1,16 @@
 package com.mike.moviehubdemo.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.mike.moviehubdemo.model.Movie
 
 @Composable
 fun MovieDetailScreen(movie: Movie){
-    Text(text="Movie Detail Screen")
-    Text(text="${movie.id}")
-    Text(text="${movie.title}")
+    Column(){
+        Text(text="Movie Detail Screen")
+        Text(text="${movie.id}")
+        Text(text="${movie.title}")
+    }
+
 }
