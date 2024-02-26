6e5713faffc8c07be5953187df6047a466a78db9
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-02-26 17:37:28 -0600

    Added farvoite toggle button to Movie Detail Page

--- app/src/main/java/com/mike/moviehubdemo/components/MovieDetailCard.kt
+++ app/src/main/java/com/mike/moviehubdemo/components/MovieDetailCard.kt
@@ -8,10 +8,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
@@ -29,6 +40,11 @@ fun MovieDetailCard(
     movieItem: Movie
     //title:String
 ) {
+
+    var movieIconState:Boolean by remember{
+        mutableStateOf(false)
+    }
+
     Column(
         modifier = Modifier
             //.border(2.dp, Color.Black, shape = CircleShape)
@@ -102,6 +118,22 @@ fun MovieDetailCard(
                         overflow = TextOverflow.Ellipsis,
                         style = MaterialTheme.typography.bodySmall,
                         color = Color.White
+                    )
+                }
+                Button(
+                    onClick = {
+                        movieIconState = !movieIconState
+                    }
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(10.dp)
+                            .scale(2.5f),
+                        imageVector = if (movieIconState) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "isIconChanged",
+                        tint = Color.White
+
+
                     )
                 }
             }
