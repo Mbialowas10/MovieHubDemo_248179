0e6ffbd8b9ad38a36bffac15aae61da488c9efa5
Mbialowas10 <94670314+Mbialowas10@users.noreply.github.com>
2024-02-26 17:27:06 -0600

    Movie Detailed Page Styled

--- app/src/main/java/com/mike/moviehubdemo/components/MovieDetailCard.kt
+++ app/src/main/java/com/mike/moviehubdemo/components/MovieDetailCard.kt
@@ -1,16 +1,25 @@
 package com.mike.moviehubdemo.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.mike.moviehubdemo.model.Movie
@@ -19,22 +28,83 @@ import com.mike.moviehubdemo.model.Movie
 fun MovieDetailCard(
     movieItem: Movie
     //title:String
-){
+) {
     Column(
         modifier = Modifier
-            .border(1.dp, Color.Red, shape = RectangleShape)
+            //.border(2.dp, Color.Black, shape = CircleShape)
+            .background(color = Color.DarkGray)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            //.height(80.dp)
             .padding(5.dp)
-    ){
+    ) {
         AsyncImage(
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight(0.5f),
             model = ImageRequest.Builder(
-                LocalContext.current)
+                LocalContext.current
+            )
                 .data("https://image.tmdb.org/t/p/w500/${movieItem.posterPath}")
-                .build() ,
+                .build(),
             contentDescription = movieItem.overview
         )
-    }
+        Column(Modifier.padding(20.dp)) {
+            movieItem.originalTitle?.let {
+                Text(
+                    color = Color.White,
+                    text = it,
+                    modifier = Modifier
+                        .padding(top = 8.dp, end = 8.dp),
+                    style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                )
+            } // end Text
+            Spacer(modifier = Modifier.padding(5.dp))
+            movieItem.releaseDate?.let {
+                Text(
+                    text = "Release Date: $it",
+                    modifier = Modifier.padding(end = 8.dp),
+                    maxLines = 9,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.White
+                )
+            }
+            Spacer(modifier = Modifier.padding(5.dp))
+            movieItem.overview?.let {
+                Text(
+                    text = it,
+                    modifier = Modifier.padding(end = 8.dp),
+                    maxLines = 9,
+                    overflow = TextOverflow.Ellipsis,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.White
+                )
+            }
+            Spacer(modifier = Modifier.padding(5.dp))
+            Row {
 
+                movieItem.voteAverage?.let {
+                    Text(
+                        text = "Avg Vote: $it",
+                        modifier = Modifier.padding(end = 8.dp),
+                        maxLines = 9,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.White
+                    )
+                }
+                movieItem.voteCount?.let {
+                    Text(
+                        text = "# of Votes: $it",
+                        modifier = Modifier.padding(end = 8.dp),
+                        maxLines = 9,
+                        overflow = TextOverflow.Ellipsis,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
 }
